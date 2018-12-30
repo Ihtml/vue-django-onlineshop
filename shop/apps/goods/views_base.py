27a9acdb783b01d0938c183a93dc179fb8276433
Ihtml <2483690020@qq.com>
2018-12-30 17:42:20 +0800

    use serializers and JsonResponse

--- shop/apps/goods/views_base.py
+++ shop/apps/goods/views_base.py
@@ -16,15 +16,23 @@ class GoodsListView(View):
         :param request:
         :return:
         """
-        json_list = []
         goods = Goods.objects.all()[:10]
-        for good in goods:
-            json_dict = {}
-            json_dict['name'] = good.name
-            json_dict['category'] = good.category.name
-            json_dict['market_price'] = good.market_price
-            json_list.append(json_dict)
+        # json_list = []
+        # for good in goods:
+        #     json_dict = {}
+        #     json_dict['name'] = good.name
+        #     json_dict['category'] = good.category.name
+        #     json_dict['market_price'] = good.market_price
+        #     json_list.append(json_dict)
 
-        from django.http import HttpResponse
+        # from django.forms.models import model_to_dict
+        # for good in goods:
+        #     json_dict = model_to_dict(good)
+        #     json_list.append(json_dict)
+
         import json
-        return HttpResponse(json.dumps(json_list), content_type='application/json')
+        from django.core import serializers
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
+        from django.http import HttpResponse, JsonResponse
+        return JsonResponse(json_data, safe=False)
