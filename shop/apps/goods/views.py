ae48707a5ae8b92ae3d8bff7bea38fc8ae3ecd30
Ihtml <2483690020@qq.com>
2018-12-31 21:42:43 +0800

    use mixins and generics

--- shop/apps/goods/views.py
+++ shop/apps/goods/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from .serializers import GoodsSerializer
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
+
 from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 from .models import Goods
 
 
-class GoodsListView(APIView):
+# GenericAPIView 继承自views.APIView，并进行了一层封装
+class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
     """
-    List all goods
+    商品列表页
     """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        # many=True表示是个queryset对象,序列化为数组
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+    queryset = Goods.objects.all()[:10]
+    serializer_class = GoodsSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
     # def post(selfs, request, farmat=None):
     #     serializer = GoodsSerializer(data=request.data)
