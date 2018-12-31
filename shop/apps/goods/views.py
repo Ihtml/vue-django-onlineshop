849dbe8f498522d63709ca66993b0d9d067625b7
Ihtml <2483690020@qq.com>
2018-12-31 22:12:07 +0800

    Pagination customize

--- shop/apps/goods/views.py
+++ shop/apps/goods/views.py
@@ -4,8 +4,17 @@ from .serializers import GoodsSerializer
 from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Goods
+
+
+# 定制化分页
+class GoodsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'p' # 默认为 "next": "http://localhost:8000/goods/?page=2",
+    max_page_size = 100
 
 
 # GenericAPIView 继承自views.APIView，并进行了一层封装
@@ -13,5 +22,6 @@ class GoodsListView(generics.ListAPIView):
     """
     商品列表页
     """
-    queryset = Goods.objects.all()[:10]
+    queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -143,6 +143,8 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
