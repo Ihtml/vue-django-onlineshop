db9f7345b7ae1ce9986df1dd08c8f3887c6a4373
Ihtml <2483690020@qq.com>
2019-01-03 23:05:06 +0800

    search_fields

--- shop/apps/goods/filters.py
+++ shop/apps/goods/filters.py
@@ -12,6 +12,8 @@ class GoodsFilter(django_filters.rest_framework.FilterSet):
     """
     pricemin = django_filters.NumberFilter(field_name='shop_price', help_text="最低价格",lookup_expr='gte')
     pricemax = django_filters.NumberFilter(field_name='shop_price', help_text="最高价格", lookup_expr='lte')
+    # 模糊查询
+    # name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Goods
--- shop/apps/goods/views.py
+++ shop/apps/goods/views.py
@@ -4,6 +4,7 @@ from .serializers import GoodsSerializer
 from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -28,8 +29,9 @@ class GoodsListViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Goods.objects.all()
     pagination_class = GoodsPagination
     serializer_class = GoodsSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filter_class = GoodsFilter
+    search_fields = ('name', 'goods_brief', 'goods_desc')
 
     # def get_queryset(self):
     #     queryset = Goods.objects.all()
