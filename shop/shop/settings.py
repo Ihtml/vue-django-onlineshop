d6dea0ce879f6f47ce5193ffb152f673a9411497
Ihtml <2483690020@qq.com>
2019-01-04 22:43:09 +0800

    CORS_ORIGIN_ALLOW_ALL

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'xadmin',
     'rest_framework',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'shop.urls'
 
 TEMPLATES = [
