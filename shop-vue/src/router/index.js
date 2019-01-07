62514999eb5e98d1b02e6b0fbfebaca1b3f54ecb
Ihtml <2483690020@qq.com>
2019-01-07 23:16:20 +0800

    add router

--- shop-vue/src/router/index.js
+++ shop-vue/src/router/index.js
@@ -1,4 +1,18 @@
 import Vue from 'vue'
-import Router from 'vue-router';
+import Router from 'vue-router'
 
-Vue.use(Router);
+Vue.use(Router)
+
+//公共部分
+import app from '../views/app/app'
+
+import store from '../store/store'
+
+const router = new Router({
+    routes: [{
+        path: '/app',
+        component: app
+,    }]
+})
+
+export default router
