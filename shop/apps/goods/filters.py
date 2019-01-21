53514bb37f3b00ca185211a40c62363a56cbbdbf
Ihtml <2483690020@qq.com>
2019-01-21 22:50:36 +0800

    top_category_filter

--- shop/apps/goods/filters.py
+++ shop/apps/goods/filters.py
@@ -3,6 +3,8 @@ __author__ = 'ife'
 __date__ = '2019-01-02 22:40'
 
 import django_filters
+from django.db.models import Q
+
 from .models import Goods
 
 
@@ -14,6 +16,12 @@ class GoodsFilter(django_filters.rest_framework.FilterSet):
     pricemax = django_filters.NumberFilter(field_name='shop_price', help_text="最高价格", lookup_expr='lte')
     # 模糊查询
     # name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    # 自定义filter,查找第一类别下面所有商品
+    top_category = django_filters.NumberFilter(method='top_category_filter')
+
+    def top_category_filter(self, queryset, name, value):
+        return queryset.filter(Q(category_id=value)|Q(category__parent_category_id=value)|Q(category__parent_category_id=value)|Q(category__parent_category__parent_category_id=value))
 
     class Meta:
         model = Goods
--- shop/apps/goods/views.py
+++ shop/apps/goods/views.py
@@ -15,9 +15,9 @@ from .serializers import GoodsSerializer, CategorySerializer
 
 # 定制化分页
 class GoodsPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 12
     page_size_query_param = 'page_size'
-    page_query_param = 'p' # 默认为 "next": "http://localhost:8000/goods/?page=2",
+    page_query_param = 'page' # 默认为 "next": "http://localhost:8000/goods/?page=2",
     max_page_size = 100
 
 
@@ -32,7 +32,7 @@ class GoodsListViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
     filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     filter_class = GoodsFilter
     search_fields = ('name', 'goods_brief', 'goods_desc')
-    ordering_fields = ('sold_num', 'add_time')
+    ordering_fields = ('sold_num', 'shop_price')
 
 
 # RetrieveModelMixin 获取详情
