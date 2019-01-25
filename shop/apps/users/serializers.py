ad82d1561cd43921a772ffa4ec59d1d73a579be5
Ihtml <2483690020@qq.com>
2019-01-25 22:22:17 +0800

    User Serializer

--- shop/apps/users/serializers.py
+++ shop/apps/users/serializers.py
@@ -14,7 +14,7 @@ from .models import VerifyCode
 User = get_user_model()
 
 
-# 不用modelserializer的原因是：VerifyCode中code是必填字段，这里只用到手机号
+# 不用modelserializer的原因是：VerifyCode中code是必填字段，这里只用验证手机号
 class SmsSerializer(serializers.Serializer):
     mobile = serializers.CharField(max_length=11)
 
@@ -38,3 +38,27 @@ class SmsSerializer(serializers.Serializer):
             raise serializers.ValidationError("距离上次发送未超过60s")
 
         return mobile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(required=True, max_length=4, min_length=4)
+
+    def validate_code(self, code):
+        # 在ModelSerializer中, self.initial_data为用户前端传进来的值, 这里username等价于mobile
+        verify_recodes = VerifyCode.objects.filter(mobile=self.initial_data['username']).order_by("add_time")
+        if verify_recodes:
+            last_records = verify_recodes[0]
+            five_mintes_ago = datetime.now() - timedelta(hours=0, minutes=5, seconds=0)
+            if five_mintes_ago < last_records.add_time:
+                raise serializers.ValidationError("验证码过期")
+
+            if last_records.code != code:
+                raise serializers.ValidationError("验证码错误")
+
+        else:
+            raise serializers.ValidationError("验证码错误")
+
+    class Meta:
+        model = User
+        # 这里的User用的是userProfile 继承的是Django自带的User username是必填字段
+        fileds = ("username", "code", "mobile")
